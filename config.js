cb79c5625d3f4fc60ac434b6b2466d6008073447
janaki3128 <79641763+janaki3128@users.noreply.github.com>
2022-05-09 18:46:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6W5xrfBv7TsP9_lO2gb0tQ_l4-AuqaY8",
+  authDomain: "story-telling-app-8d131.firebaseapp.com",
+  projectId: "story-telling-app-8d131",
+  storageBucket: "story-telling-app-8d131.appspot.com",
+  messagingSenderId: "898194929207",
+  appId: "1:898194929207:web:1e4ed752f776d4567131b2"
+  };
